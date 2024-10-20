7de4fbf7e92cce79517f302858d8109724c236d0
moaber231 <csd4861@csd.uoc.gr>
2024-10-20 16:45:09 +0300

    Initial commit

---
+++ Target/OltkpOAPjRPGF/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MALWARE_PLATFORM "www.alphaxiom.com"
+
+int main(void) {
+	printf("Hacking your computer...\n");
+	printf("Deleting your files...\n");
+	printf("Downloading malware from %s\n", MALWARE_PLATFORM);
+
+	if (1) {
+		printf("Hacking your phone...\n");
+		printf("Sending your data to https://biawwer.com\n");
+	}
+
+	return 0;
+}
